deef78275d363300ce0f73bae352b111f9ad9a63
chanext <21103847@usc.edu.ph>
2023-01-26 22:05:31 +0800

    Files for the Angular

---
+++ Angular_Files-master/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegisterComponent,
+    EmployeeComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
